e04ac5958417bf2e791e57433bd5b62d78b98adf
scrazyakai <2145303367@qq.com>
2025-07-13 19:08:26 +0800

    添加路由守护

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 import BasicLayout from "./layouts/BasicLayout.vue";
+const route = useRoute();
 </script>
 
 <template>
-  <BasicLayout />
+  <BasicLayout v-if="route.meta.showTabbar !== false" />
+  <router-view v-else />
 </template>
 
 <style>
